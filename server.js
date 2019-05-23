659e5d6cfcf35045613cddcb4d37291ecae85d58
Fernando Medeiros <fernando_m_n@hotmail.com>
2019-05-23 16:27:48 -0300

    mini-curso finalizado

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const webpack = require('webpack')
+const webpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+
+const port = 3000
+
+new webpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: { colors: true }
+}).listen(port, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Ouvindo na http://localhost:' + port)
+})
---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+    render: function () {
+        return <h1>Aplicação</h1>
+    }
+});
+
+
+  
+export default App
